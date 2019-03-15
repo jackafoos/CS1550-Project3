1f4ce6e3a03c40b08448e25b5072b82b3c28c64d
= <jem319@pitt.edu>
2019-03-15 11:26:05 -0400

    Initial Commit.
    Signed-off-by: = <jem319@pitt.edu>

---
+++ vmsim.java
@@ -0,0 +1,12 @@
+public class vmsim {
+  public static void main(String[]args){
+    // TODO: Get program Arguments
+    // TODO: Select Algorithm
+    // TODO: Run Selected Algorithm
+    // TODO: Print to Screen
+  }
+
+  // TODO: Opt
+  // TODO: FIFO
+  // TODO: Aging
+}
